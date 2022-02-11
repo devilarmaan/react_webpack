b974ae8d68c8e67caddbccf401a72d8e197df8b5
devilarmaan <devilarmaan63@gmail.com>
2022-02-11 12:36:12 +0600

    React with webpack and babel

---
+++ README.md
@@ -0,0 +1,2 @@
+# React setup with webpack 
+https://dev.to/deepanjangh/react-setup-with-webpack-for-beginners-2a8k
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+return (
+    <h1>Hi all this is webpack react!</h1>
+);
+}
+
+export default Home;
